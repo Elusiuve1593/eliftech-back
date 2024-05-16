1472b3db68eca622c84b9a7be5f1ff488211b423
Roma <elusiuve15.09.93@icloud.com>
2024-05-16 18:49:11 +0300

    --fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ConfigureModule } from 'src/config/config.module';
 import { EventsModule } from './modules/events/events.module';
 import { RegistrationModule } from './modules/registration/registration.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigureModule, EventsModule, RegistrationModule],
+  imports: [
+    EventsModule,
+    RegistrationModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://elusiuve150993:CkeV6VyzgYOUP220@elif.axp2dbn.mongodb.net/',
+    ),
+  ],
 })
 export class AppModule {}
